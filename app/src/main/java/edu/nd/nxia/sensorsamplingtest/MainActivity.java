7b38233e8dbaf4b36d5b7ae316421e91286f1263
Ning Xia <nxia@nd.edu>
2015-07-27 11:01:16 -0400

    Use thread to insert batch records

--- app/src/main/java/edu/nd/nxia/sensorsamplingtest/MainActivity.java
+++ app/src/main/java/edu/nd/nxia/sensorsamplingtest/MainActivity.java
@@ -2,6 +2,8 @@ package edu.nd.nxia.sensorsamplingtest;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -25,11 +27,14 @@ import edu.nd.nxia.sensorsamplingtest.database.CimonDatabaseAdapter;
 public class MainActivity extends Activity implements SensorEventListener, View.OnClickListener {
 
     private static final String TAG = "NDroid";
+    private static final String SHARED_PREFS = "CimonSharedPrefs";
+    private static final String PREF_VERSION = "version";
     private static final int SUPPORTED = 1;
     private static final int ACCEL_METRICS = 4;
     private static final int GYRO_METRICS = 4;
     private static final int BARO_METRICS = 1;
 
+    private Context context;
     private TextView textView;
     private Button button;
 
@@ -55,6 +60,7 @@ public class MainActivity extends Activity implements SensorEventListener, View.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        context = getApplicationContext();
         textView = (TextView) findViewById(R.id.text_view);
         button = (Button) findViewById(R.id.start_button);
         button.setOnClickListener(this);
@@ -65,57 +71,72 @@ public class MainActivity extends Activity implements SensorEventListener, View.
         mBarometer = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
 
         insertDatabaseEntries();
-        registerSensors();
-        dataList = new ArrayList<>();
     }
 
     void insertDatabaseEntries() {
-        if (DebugLog.DEBUG) Log.d(TAG, "AccelerometerService.insertDatabaseEntries - insert entries");
-        Context context = getApplicationContext();
+        SharedPreferences appPrefs = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        int storedVersion = appPrefs.getInt(PREF_VERSION, -1);
+        int appVersion = -1;
+        try {
+            appVersion = getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (DebugLog.DEBUG) Log.d(TAG, "NDroidAdmin.onCreate - appVersion:" + appVersion +
+                " storedVersion:" + storedVersion);
         database = CimonDatabaseAdapter.getInstance(context);
+        if (appVersion > storedVersion) {
+            new Thread(new Runnable() {
+                public void run() {
 
-        // Accelerometer
-        // insert metric group information in database
-        String[] metrics = {"X", "Y", "Z", "Magnitude"};
-        database.insertOrReplaceMetricInfo(Metrics.ACCELEROMETER, "Accelerometer", mAccelerometer.getName(),
-                SUPPORTED, mAccelerometer.getPower(), mAccelerometer.getMinDelay()/1000,
-                mAccelerometer.getMaximumRange() + " " + context.getString(R.string.units_ms2),
-                mAccelerometer.getResolution() + " " + context.getString(R.string.units_ms2),
-                Metrics.TYPE_SENSOR);
-        // insert information for metrics in group into database
-        for (int i = 0; i < ACCEL_METRICS; i++) {
-            database.insertOrReplaceMetrics(Metrics.ACCELEROMETER + i, Metrics.ACCELEROMETER, metrics[i],
-                    context.getString(R.string.units_ms2), mAccelerometer.getMaximumRange());
+                    if (DebugLog.DEBUG) Log.d(TAG, "AccelerometerService.insertDatabaseEntries - insert entries");
+                    // Accelerometer
+                    // insert metric group information in database
+                    String[] metrics = {"X", "Y", "Z", "Magnitude"};
+                    database.insertOrReplaceMetricInfo(Metrics.ACCELEROMETER, "Accelerometer", mAccelerometer.getName(),
+                            SUPPORTED, mAccelerometer.getPower(), mAccelerometer.getMinDelay()/1000,
+                            mAccelerometer.getMaximumRange() + " " + context.getString(R.string.units_ms2),
+                            mAccelerometer.getResolution() + " " + context.getString(R.string.units_ms2),
+                            Metrics.TYPE_SENSOR);
+                    // insert information for metrics in group into database
+                    for (int i = 0; i < ACCEL_METRICS; i++) {
+                        database.insertOrReplaceMetrics(Metrics.ACCELEROMETER + i, Metrics.ACCELEROMETER, metrics[i],
+                                context.getString(R.string.units_ms2), mAccelerometer.getMaximumRange());
+                    }
+
+                    // Gyroscope
+                    // insert metric group information in database
+                    database.insertOrReplaceMetricInfo(Metrics.GYROSCOPE, "Gyroscope", mGyroscope.getName(),
+                            SUPPORTED, mGyroscope.getPower(), mGyroscope.getMinDelay()/1000,
+                            mGyroscope.getMaximumRange() + " " + context.getString(R.string.units_rads),
+                            mGyroscope.getResolution() + " " + context.getString(R.string.units_rads),
+                            Metrics.TYPE_SENSOR);
+                    // insert information for metrics in group into database
+                    for (int i = 0; i < GYRO_METRICS; i++) {
+                        database.insertOrReplaceMetrics(Metrics.GYROSCOPE + i, Metrics.GYROSCOPE, metrics[i],
+                                context.getString(R.string.units_rads), mGyroscope.getMaximumRange());
+                    }
+
+                    // Barometer
+                    // insert metric group information in database
+                    database.insertOrReplaceMetricInfo(Metrics.ATMOSPHERIC_PRESSURE, "Pressure", mBarometer.getName(),
+                            SUPPORTED, mBarometer.getPower(), mBarometer.getMinDelay() / 1000,
+                            mBarometer.getMaximumRange() + " " + context.getString(R.string.units_hpa),
+                            mBarometer.getResolution() + " " + context.getString(R.string.units_hpa),
+                            Metrics.TYPE_SENSOR);
+                    // insert information for metrics in group into database
+                    database.insertOrReplaceMetrics(Metrics.ATMOSPHERIC_PRESSURE, Metrics.ATMOSPHERIC_PRESSURE, "Atmosphere pressure",
+                            context.getString(R.string.units_hpa), mBarometer.getMaximumRange());
+                }
+            }).start();
+            SharedPreferences.Editor editor = appPrefs.edit();
+            editor.putInt(PREF_VERSION, appVersion);
+            editor.commit();
         }
-
-        // Gyroscope
-        // insert metric group information in database
-        database.insertOrReplaceMetricInfo(Metrics.GYROSCOPE, "Gyroscope", mGyroscope.getName(),
-                SUPPORTED, mGyroscope.getPower(), mGyroscope.getMinDelay()/1000,
-                mGyroscope.getMaximumRange() + " " + context.getString(R.string.units_rads),
-                mGyroscope.getResolution() + " " + context.getString(R.string.units_rads),
-                Metrics.TYPE_SENSOR);
-        // insert information for metrics in group into database
-        for (int i = 0; i < GYRO_METRICS; i++) {
-            database.insertOrReplaceMetrics(Metrics.GYROSCOPE + i, Metrics.GYROSCOPE, metrics[i],
-                    context.getString(R.string.units_rads), mGyroscope.getMaximumRange());
-        }
-
-        // Barometer
-        // insert metric group information in database
-        database.insertOrReplaceMetricInfo(Metrics.ATMOSPHERIC_PRESSURE, "Pressure", mBarometer.getName(),
-                SUPPORTED, mBarometer.getPower(), mBarometer.getMinDelay() / 1000,
-                mBarometer.getMaximumRange() + " " + context.getString(R.string.units_hpa),
-                mBarometer.getResolution() + " " + context.getString(R.string.units_hpa),
-                Metrics.TYPE_SENSOR);
-        // insert information for metrics in group into database
-        database.insertOrReplaceMetrics(Metrics.ATMOSPHERIC_PRESSURE, Metrics.ATMOSPHERIC_PRESSURE, "Atmosphere pressure",
-                context.getString(R.string.units_hpa), mBarometer.getMaximumRange());
-
     }
 
     private void registerSensors() {
-        int sensorDelay = SensorManager.SENSOR_DELAY_GAME;
+        int sensorDelay = SensorManager.SENSOR_DELAY_FASTEST;
         mSensorManager.registerListener(this, mAccelerometer, sensorDelay);
         mSensorManager.registerListener(this, mGyroscope, sensorDelay);
         mSensorManager.registerListener(this, mBarometer, sensorDelay);
@@ -194,7 +215,13 @@ public class MainActivity extends Activity implements SensorEventListener, View.
             }
 
             if (dataList.size() >= BATCH_SIZE) {
-                database.insertBatchGroupData(monitorId, (ArrayList<DataEntry>) dataList);
+                final ArrayList<DataEntry> dl = new ArrayList<>(dataList);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        database.insertBatchGroupData(monitorId, dl);
+                    }
+                }).start();
                 dataList.clear();
             }
         }
@@ -236,7 +263,8 @@ public class MainActivity extends Activity implements SensorEventListener, View.
     }
 
     private void startRecording() {
-        startTime = System.currentTimeMillis();
+        dataList = new ArrayList<>();
+        registerSensors();
         numAccelerometer = 0;
         numGyroscope = 0;
         numBarometer = 0;
@@ -244,9 +272,11 @@ public class MainActivity extends Activity implements SensorEventListener, View.
         final long curTime = System.currentTimeMillis();
         final long upTime = SystemClock.uptimeMillis();
         monitorId = database.insertMonitor(curTime - upTime);
+        startTime = System.currentTimeMillis();
     }
 
     private void stopRecording() {
+        mSensorManager.unregisterListener(this);
         endTime = System.currentTimeMillis();
         double offset = (endTime - startTime) / 1000.0;
         double rateAccelerometer = numAccelerometer / offset;
@@ -264,7 +294,6 @@ public class MainActivity extends Activity implements SensorEventListener, View.
         if (dataList.size() > 0) {
             database.insertBatchGroupData(monitorId, (ArrayList<DataEntry>) dataList);
         }
-        mSensorManager.unregisterListener(this);
     }
 
 }
