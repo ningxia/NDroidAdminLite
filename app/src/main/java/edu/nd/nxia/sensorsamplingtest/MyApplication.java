597ed70256cbefce61ac81e86361d3bd729b4ee4
Ning Xia <nxia@nd.edu>
2015-07-27 21:16:16 -0400

    Add MyApplication class.

---
+++ app/src/main/java/edu/nd/nxia/sensorsamplingtest/MyApplication.java
@@ -0,0 +1,27 @@
+package edu.nd.nxia.sensorsamplingtest;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Class to allow global access to application context within app.
+ * @author ningxia
+ *
+ */
+public class MyApplication extends Application {
+    private static Context context;
+
+    @Override
+    public void onCreate(){
+        MyApplication.context = getApplicationContext();
+    }
+
+    /**
+     * Get application context.
+     * @return   context of application
+     */
+    public static Context getAppContext() {
+        return context;
+    }
+
+}
